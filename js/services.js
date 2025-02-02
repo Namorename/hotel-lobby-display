d1690cde6e01d624cabcb8d7da405db6222adeec
Namorename <167243887+Namorename@users.noreply.github.com>
2025-02-02 16:08:03 +0100

    Animations

--- js/services.js
+++ js/services.js
@@ -1,5 +1,5 @@
 export const services = [
-    { name: "Default", slides: ["images/default1.jpg", "images/default2.jpg", "images/default3.jpg"] },
+    { name: "Default", slides: ["images/default1.webp", "images/default2.webp", "images/default3.webp"] },
     { name: "Service 1", slides: ["images/service1_1.jpg", "images/service1_2.jpg"] },
     { name: "Service 2", slides: ["images/service2_1.jpg", "images/service2_2.jpg"] },
     { name: "Service 3", slides: ["images/service3_1.jpg", "images/service3_2.jpg"] },
